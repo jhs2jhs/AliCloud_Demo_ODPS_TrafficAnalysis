ed904c7ca17aaa019fd377665f072e06e69e951c
Jianhua Shao <psxjs4@nottingham.ac.uk>
2019-03-25 17:46:01 +0000

    Create config_mysql_in.sql

---
+++ config_mysql_in.sql
@@ -0,0 +1,8 @@
+Data Source Type: ApasaraDB for RDS
+Data Source Name: rds_workshop_log
+Description：rds log ingest
+RDS Instance ID: rm-bp1z69dodhh85z9qa
+RDS Instance Account: 1156529087455811
+Database name: workshop
+Username: workshop
+Password: workshop#2017
