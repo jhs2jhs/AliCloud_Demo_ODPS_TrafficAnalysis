200c1f13d848de06a5a2f2359d4c48261db770b7
Jianhua Shao <psxjs4@nottingham.ac.uk>
2019-03-25 07:53:00 +0000

    Create sql_dim_user.sql

---
+++ sql_dim_user.sql
@@ -0,0 +1,16 @@
+--DROP TABLE IF EXISTS dim_user_dd;
+CREATE TABLE IF NOT EXISTS dim_user_dd (
+    uid STRING COMMENT 'user ID',
+    gender STRING COMMENT 'gender',
+    age_range STRING COMMENT 'age range, e.g. 30-40 year old',
+    zodiac STRING COMMENT 'zodiac'
+)
+PARTITIONED BY (
+    dt STRING
+);
+
+-- DML
+INSERT OVERWRITE TABLE dim_user_dd PARTITION (dt=${bdp.system.bizdate})
+SELECT uid, gender, age_range, zodiac
+FROM ods_s_user_dd
+WHERE dt = ${bdp.system.bizdate};
